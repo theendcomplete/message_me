d64e2e795bcf0c18811088ba28cbbab0116ae02d
Nikkie Grom <nickonizator@gmail.com>
2019-03-29 22:52:41 +0300

    messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_050147) do
+ActiveRecord::Schema.define(version: 2019_03_29_194421) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
